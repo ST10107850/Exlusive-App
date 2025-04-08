60ef1084223a8bbefe2c92ee7441c550e44e8296
ST10107850 <ST10107850@rcconnect.edu.za>
2025-04-08 16:30:02 +0200

    Printing only hello world

--- backend/app.ts
+++ backend/app.ts
@@ -1,41 +1,49 @@
 import express from "express";
-import cookieParser from "cookie-parser";
-import { dbConnection } from "./config/dbConnection";
-import { errorHandle, notFound } from "./middleware/errorMiddleware";
-import userRoute from "./routes/userRoute";
-import categoryRoute from "./routes/categoryRoute";
-import productRoute from "./routes/productRoute";
-import cartRoute from "./routes/cartRoute";
-import orderRoute from "./routes/orderRoute";
-import bodyParser from "body-parser";
-import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import { dbConnection } from "./config/dbConnection";
+// import { errorHandle, notFound } from "./middleware/errorMiddleware";
+// import userRoute from "./routes/userRoute";
+// import categoryRoute from "./routes/categoryRoute";
+// import productRoute from "./routes/productRoute";
+// import cartRoute from "./routes/cartRoute";
+// import orderRoute from "./routes/orderRoute";
+// import bodyParser from "body-parser";
+// import cors from "cors";
+import dotenv from "dotenv"
 
+dotenv.config();
 
 
 const app = express();
-app.use(cookieParser());
-
-app.use(cors())
-
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-
-app.use("/api/users", userRoute);
-app.use("/api/category", categoryRoute);
-app.use("/api/product", productRoute);
-app.use("/api/cart", cartRoute);
-app.use("/api/orders", orderRoute);
-
-app.use(notFound);
-app.use(errorHandle);
 const PORT = process.env.PORT || 5000;
+
+app.get("/api/message", (req, res) => {
+  res.send("Hello World");
+});
+
+// app.use(cookieParser());
+
+// app.use(cors())
+
+// app.use(express.json({ limit: "50mb" }));
+// app.use(express.urlencoded({ limit: "50mb", extended: true }));
+// app.use(bodyParser.json({ limit: "50mb" }));
+// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+// app.use("/api/users", userRoute);
+// app.use("/api/category", categoryRoute);
+// app.use("/api/product", productRoute);
+// app.use("/api/cart", cartRoute);
+// app.use("/api/orders", orderRoute);
+
+// app.use(notFound);
+// app.use(errorHandle);
+
 
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
-  dbConnection();
+  // dbConnection();
 });
 
 
