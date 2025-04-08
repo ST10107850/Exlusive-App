141001e64ac9f04eb8a1f00c3675c54cac57e66e
ST10107850 <ST10107850@rcconnect.edu.za>
2025-04-08 10:59:19 +0200

    Adding body parser

--- backend/app.ts
+++ backend/app.ts
@@ -7,13 +7,17 @@ import categoryRoute from "./routes/categoryRoute";
 import productRoute from "./routes/productRoute";
 import cartRoute from "./routes/cartRoute";
 import orderRoute from "./routes/orderRoute";
+import bodyParser from "body-parser";
 
 
 
 const app = express();
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use("/api/users", userRoute);
 app.use("/api/category", categoryRoute);
